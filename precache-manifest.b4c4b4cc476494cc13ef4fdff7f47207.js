30b9a5dc6a53316a8c70f4dc3d5cc71cdbedd4f8
dsolanki77 <no-reply>
2020-06-17 21:54:28 -0400

    Updates

---
+++ precache-manifest.b4c4b4cc476494cc13ef4fdff7f47207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f23074d5ddd2f84385e3b28cdec659",
+    "url": "/blm-test/index.html"
+  },
+  {
+    "revision": "1bddbdbad61aa08e4b6f",
+    "url": "/blm-test/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a537838cd2acc8df7e5f",
+    "url": "/blm-test/static/css/main.de464cbb.chunk.css"
+  },
+  {
+    "revision": "1bddbdbad61aa08e4b6f",
+    "url": "/blm-test/static/js/2.ca187125.chunk.js"
+  },
+  {
+    "revision": "aa2937bbd23b56ebb8238c918e07b5d0",
+    "url": "/blm-test/static/js/2.ca187125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a537838cd2acc8df7e5f",
+    "url": "/blm-test/static/js/main.702e7046.chunk.js"
+  },
+  {
+    "revision": "6cac332aff19b7b7d544",
+    "url": "/blm-test/static/js/runtime-main.f5a17f0b.js"
+  }
+]);
